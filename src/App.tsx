80771ec02276256c6c251df6338da2f628202049
Alice Morozova <alicemorozovafront@gmail.com>
2024-04-25 03:43:12 +0300

    added configuration of firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import AppRouter from "./AppRouter"
 import "styles/globals.scss"
 import "./App.css"
 import "ag-grid-community/styles/ag-grid.css"
 import "ag-grid-community/styles/ag-theme-balham.css"
+import AppRouter from "./AppRouter"
 
 function App() {
   return <AppRouter />
