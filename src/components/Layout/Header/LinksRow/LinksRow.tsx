1f00791fb87c22efed93d33809155ac1b00244f6
Alice Morozova <alicemorozovafront@gmail.com>
2024-05-02 00:09:45 +0300

    added burger menu for mobile navbar

--- src/components/Layout/Header/LinksRow/LinksRow.tsx
+++ src/components/Layout/Header/LinksRow/LinksRow.tsx
@@ -1,23 +1,51 @@
 import { FC } from "react"
 import { LinkT } from "../links"
 import { Link } from "components"
+import { Close, Menu } from "ui/icons"
+import cn from "classnames"
 import s from "./LinksRow.module.scss"
 
 interface LinksRowI {
   links: LinkT[]
   gap: number
+  isMin: boolean
+  onSwapMin: () => void
 }
 
-export const LinksRow: FC<LinksRowI> = ({ links, gap }) => {
+export const LinksRow: FC<LinksRowI> = ({ links, gap, onSwapMin, isMin }) => {
   return (
-    <div className={s.linksRowCnt} style={{ gap }}>
-      {links.map((link) => {
-        return (
-          <Link key={link.title} to={link.to}>
-            {link.title}
-          </Link>
-        )
-      })}
+    <>
+      <div className={s.linksRowCntDesktop} style={{ gap }}>
+        {links.map((link) => {
+          return (
+            <Link key={link.title} to={link.to}>
+              {link.title}
+            </Link>
+          )
+        })}
+      </div>
+      <div className={s.linksRowCntMobile} style={{ gap }}>
+        {isMin ? <Menu className={s.menu} onClick={onSwapMin} /> : <Close className={s.closeIcon} onClick={onSwapMin} />}
+        <MobileLinksList links={links} onClick={onSwapMin} className={isMin ? s.isMin : s.notIsMin} />
+      </div>
+    </>
+  )
+}
+
+interface MobileLinksListI {
+  links: LinkT[]
+  onClick: () => void
+  className?: string
+}
+
+const MobileLinksList: FC<MobileLinksListI> = ({ links, onClick, className }) => {
+  return (
+    <div className={cn(className, s.mobileList)}>
+      {links.map((link) => (
+        <Link key={link.title} to={link.to} onClick={onClick}>
+          {link.title}
+        </Link>
+      ))}
     </div>
   )
 }
